46220d06c7054090eb57e21d0b829ba4a8541d41
Guilherme Menezes <gmazevedo.2022@gmail.com>
2025-05-29 08:48:36 -0300

    vou criar uma nova branch esqueci

---
+++ doutor-agenda/src/app/authentication/page.tsx
@@ -0,0 +1,9 @@
+const AuthenticationPage = () => {
+  return (
+    <div className="h-screen w-screen items-center justify-center">
+      AuthenticationPage
+    </div>
+  );
+};
+
+export default AuthenticationPage;
